41aa0fd02653bf65377db9cb213f53fdf8d21425
lya <lindaielyrodrigues@gmail.com>
2022-08-10 16:35:03 -0300

    imagens dos produtos na index

--- index.php
+++ index.php
@@ -11,9 +11,308 @@
 				<li><a href="">QUADROS</a></li>
 			</ul>
 		</nav>
-		<div class="container-produtos">
-			<section class="produtos">
-				vitrine
+		<div class="container-produtos" id="container-produtos">
+			<div class="produtos-filtro" id="produtos-filtro">
+				<select name="filtro-btn" id="filtro-btn">
+					<option disable>Ordenar por</option>
+					<option value="">Mais vendidas</option>
+					<option value="">Menor preço</option>
+					<option value="">Maior preço</option>
+					<option value="">Promoções</option>
+					<option value="">Novidades</option>
+				</select>
+			</div>
+			<section class="sessao-produtos">
+				<div class="container-sessao-produtos">
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca25-dc.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Batman</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca26-marvel.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Homem de ferro</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca27-marvel.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Homem de ferro</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca28-marvel.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Capitão américa</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca29-marvel.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Marvel</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca30-marvel.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Thanos</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca31-marvel.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Capitão américa</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca32-marvel.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Capitã marvel</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca33-marvel.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Homem - aranha</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca34-marvel.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Homem - aranha</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca35-lgbt.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">A ovelha colorida da família</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca36-lgbt.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Orgulho</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca37-lgbt.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Love is love.</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca38-lgbt.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Toda forma de amor valerá.</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca39-lgbt.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Consideramos justa toda forma de amor.</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca40-lgbt.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Trans is beautiful</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca41-lgbt.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">O amor é lindo demais!</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="card-thumb">
+						<a href="">
+							<img src="img/caneca42-lgbt.jpg" id="card-img" alt="">
+						</a>
+						<div class="card-thumb-info">
+							<a href="">Todo amor é bonito, feio é não amar!</a>
+							<div class="card-thumb-detalhes">
+								<div class="card-thumb-precos">
+									<span>R$ 39,90</span>
+								</div>
+								<div class="card-thumb-parcial-precos">
+									ou 3x R$ 9,90 sem juros
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</section>
 		</div>
 		<aside class="feed">
@@ -24,7 +323,15 @@
 				<img src="" class="imagem-feed" alt="">
 				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur possimus ea, exercitationem aliquam eum odio esse excepturi dolores beatae, blanditiis laudantium. Deserunt ipsam facere eius quis illo voluptatum natus ex.</p> <br>
 				<img src="" class="imagem-feed" alt="">
-				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur possimus ea, exercitationem aliquam eum odio esse excepturi dolores beatae, blanditiis laudantium. Deserunt ipsam facere eius quis illo voluptatum natus ex.</p>
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur possimus ea, exercitationem aliquam eum odio esse excepturi dolores beatae, blanditiis laudantium. Deserunt ipsam facere eius quis illo voluptatum natus ex.</p> <br>
+				<img src="" class="imagem-feed" alt="">
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur possimus ea, exercitationem aliquam eum odio esse excepturi dolores beatae, blanditiis laudantium. Deserunt ipsam facere eius quis illo voluptatum natus ex.</p> <br>
+				<img src="" class="imagem-feed" alt="">
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur possimus ea, exercitationem aliquam eum odio esse excepturi dolores beatae, blanditiis laudantium. Deserunt ipsam facere eius quis illo voluptatum natus ex.</p> <br>
+				<img src="" class="imagem-feed" alt="">
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur possimus ea, exercitationem aliquam eum odio esse excepturi dolores beatae, blanditiis laudantium. Deserunt ipsam facere eius quis illo voluptatum natus ex.</p> <br>
+				<img src="" class="imagem-feed" alt="">
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur possimus ea, exercitationem aliquam eum odio esse excepturi dolores beatae, blanditiis laudantium. Deserunt ipsam facere eius quis illo voluptatum natus ex.</p> <br>
 			</div>
 		</aside>
 
